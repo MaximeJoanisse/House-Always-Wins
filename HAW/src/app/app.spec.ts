f823b29887b7baeae95d99d6905661e9ebd41ff9
MaximeJoanisse <maxjoanisse101@gmail.com>
2025-09-29 15:53:48 -0400

    reintallation of Angular

--- HAW/src/app/app.spec.ts
+++ HAW/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, House-Always-Wins');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, HAW');
   });
 });
